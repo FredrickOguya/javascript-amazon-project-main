2c1d77a9d6c0c72260e2db95832a41d30a3e184f
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2024-05-11 09:12:28 +0300

    make ad to cart button interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -49,36 +49,37 @@ products.forEach((product)=>{
                 </div>
 
                 <button class="add-to-cart-button button-primary js-add-to-cart" 
-                data-product-name="${product.name}">
+                data-product-id="${product.id}">
                     Add to Cart
                 </button>
                 </div>
     `;
 })
  document.querySelector('.js-products-grid').innerHTML = productsHTML
- document.querySelectorAll('.js-add-to-cart')
- .forEach((button)=>(
-  button.addEventListener('click',()=>{
-   const productName = button.dataset.productName;
+ 
+    document.querySelectorAll('.js-add-to-cart')
+    .forEach((button)=>{
+        button.addEventListener('click',()=>{
+        const productId = button.dataset.productId ;
 
-   let matchingItem;
+        let matchingItem;
 
-   cart.forEach((item)=> {
-    if (productName === item.productName){
-        matchingItem = item;
-    }
-   });
+        cart.forEach((item)=> {
+            if (productId === item.productId){
+                matchingItem = item;
+            }
+        });
 
-   if (matchingItem) {
-    matchingItem.quantity +=1;
-   }else{
-    cart.push({
-    productName : productName,
-    quantity : 1
-   });
-   }
+        if (matchingItem) {
+            matchingItem.quantity +=1;
+        }else{
+            cart.push({
+            productId : productId,
+            quantity : 1
+        });
+        }
 
-   
-   console.log(cart);
- });
-}));
+        
+        console.log(cart);
+        });
+    });
