a9f9fc741b050af869374a1a976a78507bc06082
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2024-05-11 09:27:57 +0300

    make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -79,7 +79,12 @@ products.forEach((product)=>{
         });
         }
 
-        
-        console.log(cart);
+        let cartQuantity = 0;
+
+        cart.forEach((item)=>{
+            cartQuantity+= item.quantity;
+        });
+
+        document.querySelector('.js-cart-quantity').innerHTML =cartQuantity;
         });
     });
