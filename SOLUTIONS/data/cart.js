250fcfe06d25b694ae99f10e2e49567297c7c751
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2024-05-20 14:31:40 +0300

    save cart in local storage

--- SOLUTIONS/data/cart.js
+++ SOLUTIONS/data/cart.js
@@ -1,12 +1,17 @@
-export let cart = [{
-    productId :
-    'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity :2
-}, {
-    productId : 
-    '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity : 1
-}];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart){
+    cart = [{
+        productId :
+        'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity :2
+    }, {
+        productId : 
+        '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity : 1
+    }];
+}
+
 
 
 export function addToCart(productId){
@@ -32,6 +37,7 @@ export function addToCart(productId){
         quantity : quatityNumber
     });
     }
+    saveToStorage();
 }
 export function removeFromCart (productId){
     const newCart = [];
@@ -42,4 +48,9 @@ export function removeFromCart (productId){
         }
     });
     cart = newCart;
+
+    saveToStorage();
 }
+function saveToStorage() {
+    localStorage.setItem('cart',JSON.stringify(cart))
+};
