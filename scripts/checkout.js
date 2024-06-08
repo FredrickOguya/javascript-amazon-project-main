d3d7dd172697b478ab2148d6e529fa521d194208
fredrick onyango <159248804+FredrickOguya@users.noreply.github.com>
2024-06-08 17:09:18 +0300

    save data and generate html for deliveryoption

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -24,10 +24,28 @@ cart.forEach((cartItem)=>{
     });
  
     
+    const deliveryOptionId = cartItem.deliveryOptionId;
+
+    let deliveryOption;
+
+    deliveryOptions.forEach((option)=> {
+      if (option.id === deliveryOptionId){
+        deliveryOption = option;
+      }
+    });
+
+    const today = dayjs();
+    const deliveryDate = today.add(
+      deliveryOption.deliveryDays,'days'
+      );
+
+      const dateString = deliveryDate.format(
+        `dddd, MMMM D`
+        );
     cartSummaryHTML+=`
     <div class="cart-item-container js-cart-item-container-${matchingproduct.id}">
             <div class="delivery-date">
-              Delivery date: Wednesday, June 15
+              Delivery date: ${dateString}
             </div>
 
             <div class="cart-item-details-grid">
