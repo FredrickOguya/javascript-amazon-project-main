ff7f6bd603660a063951f004903f77dd956dfca6
fredrick onyango <159248804+FredrickOguya@users.noreply.github.com>
2024-06-11 13:56:37 +0300

    testing

---
+++ tests/moneyTest.js
@@ -0,0 +1,37 @@
+import {formatCurrency} from '../SOLUTIONS/utils/money.js';
+
+
+console.log('test suite:format currency')
+
+console.log('converts cents into dollars')
+if(formatCurrency(2095) === '20.95'){
+    console.log('passed');
+}else{
+    console.log('failed');
+}
+
+
+console.log('works with 0')
+
+if (formatCurrency(0)=== '0.00'){
+    console.log('passed');
+}else{
+    console.log('failed');
+}
+
+console.log('rounds up to the nearest cent')
+if (formatCurrency(2000.5)=== '20.01'){
+    console.log('passed');
+}else{
+    console.log('failed');
+}
+console.log('rounding up')
+if (formatCurrency(2000.04)=== '20.00'){
+    console.log('passed')
+}else{
+    console.log('failed')
+}
+
+
+
+
