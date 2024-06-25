7a5511c76e5e252fc10484d06b12991276c260f2
fredrick onyango <159248804+FredrickOguya@users.noreply.github.com>
2024-06-25 15:44:37 +0300

    load Products in beforeAll hook

--- tests-jasmine/checkout/orderSummaryTest.js
+++ tests-jasmine/checkout/orderSummaryTest.js
@@ -1,10 +1,19 @@
 import {renderOrderSummary} from '../../scripts/checkout/orderSummary.js';
 import {loadFromStorage,cart } from "../../SOLUTIONS/data/cart.js";
+import { loadProducts } from '../../SOLUTIONS/data/products.js';
 
-const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
-        const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d'
 
 describe('test suite: renderOrderSummary',()=>{
+
+    const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
+    const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d'
+
+    beforeAll((done)=>{
+        loadProducts(()=>{
+            done();
+        });
+        
+    });
     beforeEach(()=>{
                 spyOn(localStorage,'setItem')
         document.querySelector('.js-test-container').innerHTML= 
