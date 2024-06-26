4c0c5e4322e240659e06c7b0589f3214885404e4
fredrick onyango <159248804+FredrickOguya@users.noreply.github.com>
2024-06-26 12:23:03 +0300

    Learn Promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,9 +1,55 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import {renderPaymentSummary} from "./checkout/paymentSummary.js"
 import {loadProducts} from "./../SOLUTIONS/data/products.js"
+import {loadCart} from "../SOLUTIONS/data/cart.js"
 //import '../SOLUTIONS/data/backend-practise.js'
 //import '../SOLUTIONS/data/cart-class.js';
-loadProducts(()=>{
-    renderOrderSummary()
+
+
+
+Promise.all([
+    new Promise ((resolve)=>{
+        loadProducts(()=>{
+            resolve('value1');
+        })
+    }),
+    new Promise ((resolve)=>{
+        loadCart(()=>{
+            resolve();
+        });
+    })
+]).then((values)=>{
+    console.log(values)
+    renderOrderSummary();
     renderPaymentSummary()
+})
+/*
+new Promise ((resolve)=>{
+    loadProducts(()=>{
+        resolve('value1');
+    })
+
+}).then((value)=>{
+    console.log(value)
+
+    return new Promise ((resolve)=>{
+        loadCart(()=>{
+            resolve();
+        });
+    });
+
+}).then (()=>{
+    renderOrderSummary();
+    renderPaymentSummary();
+})
+/*
+
+/*
+loadProducts(()=>{
+    loadCart(()=>{
+        renderOrderSummary();
+        renderPaymentSummary(); 
+    })
+
 });
+*/
