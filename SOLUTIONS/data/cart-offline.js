28c462a831361b31b3ada227d8792ca8c63148fd
fredrick onyango <159248804+FredrickOguya@users.noreply.github.com>
2024-06-27 06:16:13 +0300

    make changes in accessing the exercises offline and making the multiselector interactive

---
+++ SOLUTIONS/data/cart-offline.js
@@ -0,0 +1,80 @@
+export let cart;
+
+loadFromStorage();
+
+export function loadFromStorage(){
+    cart = JSON.parse(localStorage.getItem('cart'));
+
+
+   if (!cart){
+    cart = [{
+        productId :'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity :2,
+        deliveryOptionId :'1'
+    }, {
+         productId :'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity : 1,
+        deliveryOptionId :'2'
+    }];
+} 
+}
+function saveToStorage() {
+    localStorage.setItem('cart',JSON.stringify(cart));
+}
+
+
+
+export function addToCart(productId){
+    const multipleQuantity = document.querySelector(`.js-quantity-selector-${productId}`)
+
+            const multipleSelected = multipleQuantity.value
+
+            const multiSelect = Number(multipleSelected)
+    
+    
+    let matchingItem;
+    
+
+    cart.forEach((Item)=> {
+        if (productId === Item.productId){
+            matchingItem = Item;
+        }
+    });
+
+    if (matchingItem) {
+        matchingItem.quantity += multiSelect;
+    }else{
+        cart.push({
+        productId : productId,
+        quantity : multiSelect,
+        deliveryOptionId :'1'
+
+    });
+    }
+    saveToStorage();
+}
+export function removeFromCart (productId){
+    const newCart = [];
+
+    cart.forEach((cartItem)=>{
+        if (cartItem.productId !== productId) {
+            newCart.push(cartItem)
+        }
+    });
+    cart = newCart;
+
+    saveToStorage();
+}
+
+export function updateDeliveryOption (productId , deliveryOptionId){
+    let matchingItem;
+    cart.forEach((cartItem)=> {
+        if (productId === cartItem.productId){
+            matchingItem = cartItem;
+        }
+    });
+
+    matchingItem.deliveryOptionId= deliveryOptionId;
+
+    saveToStorage();
+}
---
+++ scripts/checkout-offline.js
@@ -0,0 +1,4 @@
+import { renderOrderSummary } from "./checkout/orderSummary-offline.js";
+import {renderPaymentSummary} from "./checkout/paymentSummary-offline.js"
+renderOrderSummary()
+renderPaymentSummary()
